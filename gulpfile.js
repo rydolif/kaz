8d45233481de39b14b0d100050d4d13c4fee5fbe
rudolif <rudolifrudolif@gmail.com>
2018-12-21 14:35:07 +0200

    правки

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('styles', function() {
 	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
 	.pipe(rename({ suffix: '.min', prefix : '' }))
 	.pipe(autoprefixer(['last 15 versions']))
-	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	// .pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
 	.pipe(gulp.dest('app/css'))
 	.pipe(browsersync.reload( {stream: true} ))
 });
@@ -42,10 +42,11 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',  			//----jquery
-		'app/libs/jquery.validate.js', 					//----форма
-		'app/libs/jquery.mask.min.js', 					//----форма
-		'app/libs/jquery.popupoverlay.js', 				//----модалки
-		'app/libs/slick/slick.js', 						//----слайдер
+		// 'app/libs/jquery.validate.js', 					//----форма
+		// 'app/libs/jquery.mask.min.js', 					//----форма
+		// 'app/libs/jquery.popupoverlay.js', 				//----модалки
+		'app/libs/slick/slick.js',
+		'app/libs/animation/wow.min.js', 						//----слайдер
 		// 'app/libs/swiper/swiper.min.js', 			//----слайдер
 		// 'app/libs/fancybox/jquery.fancybox.js', 		//----картінка прикліку
 		// 'app/libs/jquery.spincrement.min.js', 		//----цифри анімованні
@@ -55,7 +56,7 @@ gulp.task('js', function() {
 		])
 	.pipe(plumber())
 	.pipe(concat('scripts.min.js'))
-	.pipe(uglify()) // Mifify js (opt.)
+	// .pipe(uglify()) // Mifify js (opt.)
 	.pipe(gulp.dest('app/js'))
 	.pipe(browsersync.reload({ stream: true }))
 });
